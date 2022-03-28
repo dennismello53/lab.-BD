acb179727f1258e8e81692378770bc73a38d4682
dennismello53 <61035129+dennismello53@users.noreply.github.com>
2022-03-28 11:55:07 -0300

    Update and rename Views.sql to Aula5.sql

--- Views.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW v_descricao_onibus
-	AS SELECT Codigo as Codigo
-	FROM dbo.Viagem
-
-
-
-
---3) Criar uma View (Chamada v_descricao_onibus) que mostre o Código da Viagem, o Nome do motorista, a placa do ônibus (Formato XXX-0000), a Marca do ônibus, o Ano do ônibus e a descrição do onibus		
