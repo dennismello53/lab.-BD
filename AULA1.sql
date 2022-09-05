b2d74615571c77da70eaa87ccd2e7288b5221a9d
dennismello53 <61035129+dennismello53@users.noreply.github.com>
2022-09-04 21:09:44 -0300

    Update AULA1.sql

--- AULA1.sql
+++ AULA1.sql
@@ -6,16 +6,17 @@ CREATE TABLE motorista (
 	codigo INT NOT NULL,
 	nome VARCHAR(20),
 	naturalidade VARCHAR(15),
-	PRIMARY KEY(codigo)
+	PRIMARY KEY (codigo),
 );
+
  
 SELECT * FROM motorista;
 
 CREATE TABLE onibus (
-	placa VARCHAR(7),
+	placa VARCHAR(9),
 	marca VARCHAR(8),
 	ano INT,
-	descriçao VARCHAR(15),
+	descriÃ§ao VARCHAR(15),
 	PRIMARY KEY(placa)
 );
 
@@ -23,7 +24,7 @@ SELECT * FROM onibus;
 
 CREATE TABLE viagem(
 	codigo INT,
-	onibusPlaca VARCHAR(7),
+	onibusPlaca VARCHAR(9),
 	motoristaCodigo INT,
 	hora_de_saida INT NOT NULL CHECK(hora_de_saida >= 0),
 	hora_de_chegada INT NOT NULL CHECK(hora_de_chegada >= 0),
@@ -31,12 +32,11 @@ CREATE TABLE viagem(
 	destino VARCHAR(21),
 	PRIMARY KEY (codigo, onibusPlaca, motoristaCodigo),
 	FOREIGN KEY (onibusPlaca) REFERENCES onibus (placa),
-	FOREIGN KEY (motoristaCodigo) REFERENCES motorista (codigo)
+	FOREIGN KEY (motoristaCodigo) REFERENCES motorista (codigo),
+
 );
 
 SELECT * FROM viagem;
-
-DROP TABLE viagem
 
 SELECT CAST(codigo AS VARCHAR(6)) AS id, nome AS nome FROM motorista
 UNION
@@ -49,13 +49,33 @@ CAST(codigo AS VARCHAR(6)) AS id, nome AS nome FROM motorista
 UNION
 SELECT placa AS nome, marca as id FROM onibus
 
-SELECT id, nome
+CREATE VIEW v_descricao_onibus
+AS
+SELECT v.codigo, m.nome AS nomeMotorista,
+LEFT(v.onibusPlaca,3) + '-' + SUBSTRING(v.onibusPlaca, 4, 4) AS onibusPlaca,
+o.marca, o.ano, o.descriÃ§ao
+FROM viagem v
+LEFT JOIN onibus o
+ON v.onibusPlaca = o.placa 
+LEFT JOIN motorista m
+ON v.motoristaCodigo = m.codigo
+
+CREATE VIEW v_descricao_viagem 
+AS
+SELECT viagem.codigo, 
+LEFT(viagem.onibusPlaca,3) + '-' + SUBSTRING(viagem.onibusPlaca, 4, 4) AS onibusPlaca, 
+CAST(viagem.hora_de_saida AS varchar) + ':' + '00' AS hora_de_saida,
+CAST(viagem.hora_de_chegada AS varchar) + ':' + '00' AS hora_de_chegada,
+viagem.partida, viagem.destino FROM viagem
+
+DROP VIEW v_descricao_viagem 
+
+SELECT *
 FROM v_motorista_onibus
 
-/*CREATE VIEW v_descricao_onibus
-AS
-SELECT
-codigo.codigo_viagem AS codigoViagem, motorista.codigo AS motorista, onibus.placa AS placa_onibus, onibus.marca AS marca, onibus.ano AS onibus_ano, onibus.descriçao AS descriçao FROM motorista INNER JOIN viage
-*/
+SELECT *
+FROM v_descricao_onibus  
 
+SELECT * 
+FROM v_descricao_viagem
 
